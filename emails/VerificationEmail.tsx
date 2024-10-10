858d15915ce68a80daa7cea0fea1ec97d8154093
sachu0dev <sushil.dev.in@gmail.com>
2024-10-10 19:51:55 +0530

    UP:  verify mail template

--- emails/VerificationEmail.tsx
+++ emails/VerificationEmail.tsx
@@ -1,64 +1,161 @@
 import {
-  Html,
+  Body,
+  Container,
   Head,
-  Font,
-  Preview,
   Heading,
-  Row,
+  Html,
+  Img,
+  Link,
   Section,
   Text,
-  Button,
-} from '@react-email/components';
+} from "@react-email/components";
+import * as React from "react";
 
 interface VerificationEmailProps {
   username: string;
   otp: string;
 }
 
-export default function VerificationEmail({ username, otp }: VerificationEmailProps) {
+export default function VerificationEmail({
+  username,
+  otp,
+}: VerificationEmailProps) {
   return (
-    <Html lang="en" dir="ltr">
-      <Head>
-        <title>Verification Code</title>
-        <Font
-          fontFamily="Roboto"
-          fallbackFontFamily="Verdana"
-          webFont={{
-            url: 'https://fonts.gstatic.com/s/roboto/v27/KFOmCnqEu92Fr1Mu4mxKKTU1Kg.woff2',
-            format: 'woff2',
-          }}
-          fontWeight={400}
-          fontStyle="normal"
-        />
-      </Head>
-      <Preview>Here&apos;s your verification code: {otp}</Preview>
-      <Section>
-        <Row>
-          <Heading as="h2">Hello {username},</Heading>
-        </Row>
-        <Row>
-          <Text>
-            Thank you for registering. Please use the following verification
-            code to complete your registration:
+    <Html>
+      <Head />
+      <Body style={main}>
+        <Container style={container}>
+          <Img
+            src={`https://res.cloudinary.com/dijtcmvrm/image/upload/f_auto,q_auto/wivn3fndzqkw1rrc2ywv`}
+            width="212"
+            height="212"
+            alt="Nameless Feedback"
+            style={logo}
+          />
+          <Text style={tertiary}>Verify Your Identity</Text>
+          <Heading style={secondary}>
+            Welcome to Nameless Feedback, {username}
+          </Heading>
+          <Section style={codeContainer}>
+            <Text style={code}>{otp}</Text>
+          </Section>
+          <Text style={paragraph}>Not expecting this email?</Text>
+          <Text style={paragraph}>
+            Contact{" "}
+            <Link href="mailto:sushil.dev.in@gmail.com" style={link}>
+              sushil.dev.in@gmail.com
+            </Link>{" "}
+            if you did not request this code.
           </Text>
-        </Row>
-        <Row>
-          <Text>{otp}</Text> 
-        </Row>
-        <Row>
-          <Text>
-            If you did not request this code, please ignore this email.
-          </Text>
-        </Row>
-        {/* <Row>
-          <Button
-            href={`http://localhost:3000/verify/${username}`}
-            style={{ color: '#61dafb' }}
-          >
-            Verify here
-          </Button>
-        </Row> */}
-      </Section>
+        </Container>
+        <Text style={footer}>Securely powered by nameless.</Text>
+      </Body>
     </Html>
   );
 }
+
+VerificationEmail.PreviewProps = {
+  username: "John Doe",
+  otp: "144833",
+} as VerificationEmailProps;
+
+const main = {
+  backgroundColor: "#ffffff",
+  fontFamily: "HelveticaNeue,Helvetica,Arial,sans-serif",
+  display: "flex",
+  flexDirection: "column",
+  justifyContent: "center",
+  minHeight: "100vh",
+};
+
+const container = {
+  backgroundColor: "#ffffff",
+  border: "1px solid #eee",
+  borderRadius: "5px",
+  boxShadow: "0 5px 10px rgba(20,50,70,.2)",
+  marginTop: "20px",
+  maxWidth: "500px",
+  margin: "0 auto",
+  padding: "68px 0 130px",
+};
+
+const logo = {
+  margin: "0 auto",
+};
+
+const tertiary = {
+  color: "#0a85ea",
+  fontSize: "11px",
+  fontWeight: 700,
+  fontFamily: "HelveticaNeue,Helvetica,Arial,sans-serif",
+  height: "16px",
+  letterSpacing: "0",
+  lineHeight: "16px",
+  margin: "16px 8px 8px 8px",
+  textTransform: "uppercase" as const,
+  textAlign: "center" as const,
+};
+
+const secondary = {
+  color: "#000",
+  display: "inline-block",
+  fontFamily: "HelveticaNeue-Medium,Helvetica,Arial,sans-serif",
+  fontSize: "20px",
+  fontWeight: 500,
+  lineHeight: "24px",
+  marginBottom: "0",
+  marginTop: "0",
+  textAlign: "center" as const,
+};
+
+const codeContainer = {
+  background: "rgba(0,0,0,.05)",
+  borderRadius: "4px",
+  margin: "16px auto 14px",
+  verticalAlign: "middle",
+  width: "280px",
+};
+
+const code = {
+  color: "#000",
+  display: "inline-block",
+  fontFamily: "HelveticaNeue-Bold",
+  fontSize: "32px",
+  fontWeight: 700,
+  letterSpacing: "6px",
+  lineHeight: "40px",
+  paddingBottom: "8px",
+  paddingTop: "8px",
+  margin: "0 auto",
+  width: "100%",
+  textAlign: "center" as const,
+};
+
+const paragraph = {
+  color: "#444",
+  fontSize: "15px",
+  fontFamily: "HelveticaNeue,Helvetica,Arial,sans-serif",
+  letterSpacing: "0",
+  lineHeight: "23px",
+  padding: "0 40px",
+  margin: "0",
+  textAlign: "center" as const,
+};
+
+const link = {
+  color: "#444",
+  textDecoration: "underline",
+};
+
+const footer = {
+  color: "#000",
+  fontSize: "12px",
+  fontWeight: 800,
+  letterSpacing: "0",
+  lineHeight: "23px",
+  margin: "0",
+  marginTop: "20px ",
+  fontFamily: "HelveticaNeue,Helvetica,Arial,sans-serif",
+  textAlign: "center" as const,
+  textTransform: "uppercase" as const,
+};
