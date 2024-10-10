c1f6a7bfcad2733e9bbb76ee4dc3385f48b75ae7
sachu0dev <sushil.dev.in@gmail.com>
2024-10-10 14:34:29 +0530

    FIX: EMBED URL

--- src/app/api/embed/route.ts
+++ src/app/api/embed/route.ts
@@ -2,7 +2,7 @@ export async function GET(request: Request) {
     const { searchParams } = new URL(request.url);
     const username = searchParams.get('username');
     
-    const backendUrl = 'https://nameless-feedback.vercel.app/api'; // Replace with your actual backend URL
+    const backendUrl = 'https://nameless-feedback.vercel.app'; // Replace with your actual backend URL
   
     // Add CORS headers for cross-origin access
     const headers = {
