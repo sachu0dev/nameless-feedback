f35babd5a5cab1bacf198b231dd78dec5700fa54
Sushil Kumar <sushil.dev.in@gmail.com>
2024-10-11 00:38:59 +0530

    Update AI Prompt

--- src/app/u/[username]/page.tsx
+++ src/app/u/[username]/page.tsx
@@ -58,7 +58,7 @@ export default function SendMessage() {
     responseMimeType: "text/plain",
   };
 
-  const fixedPrompt = `Create a list of three open-ended and engaging feedback suggestions formatted as a single string. Each suggestion should be separated by '||'. These messages should be suitable for a diverse audience and can cover various aspects of performance, creativity, or improvements. Ensure the suggestions are positive, encouraging, and foster a constructive environment for sharing feedback. For example, your output could be structured like this: 'What aspects of the product do you find most valuable?||How can we improve your experience with our service?||What are some features you would love to see in the future?'. The messages should invite thoughtful responses and contribute to a welcoming dialogue.`;
+  const fixedPrompt =  "Create a list of three open-ended and engaging questions formatted as a single string. Each question should be separated by '||'. These questions are for an anonymous social messaging platform, like Qooh.me, and should be suitable for a diverse audience. Avoid personal or sensitive topics, focusing instead on universal themes that encourage friendly interaction. For example, your output should be structured like this: 'What’s a hobby you’ve recently started?||If you could have dinner with any historical figure, who would it be?||What’s a simple thing that makes you happy?'. Ensure the questions are intriguing, foster curiosity, and contribute to a positive and welcoming conversational environment.";
 
   const fetchSuggestedMessages = async () => {
     setIsSuggestLoading(true);
